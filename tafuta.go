f6f49983e61d2bf197b2a3912f5189fba77e6df5
gernest <geofreyernest@live.com>
2018-09-09 20:11:09 +0300

    add docs

--- tafuta.go
+++ tafuta.go
@@ -75,10 +75,47 @@ const (
 	// 	If there is no match, the browser will make a normal request, and will
 	// 	update the cache with the downloaded resource.
 	DefaultCache RequestCache = 1 << iota
+
+	// NoStore The browser fetches the resource from the remote server without
+	// first looking in the cache, and will not update the cache with the
+	// downloaded resource.
 	NoStore
+
+	// Reload The browser fetches the resource from the remote server without first
+	// looking in the cache, but then will update the cache with the downloaded
+	// resource.
 	Reload
+
+	// NoCache The browser looks for a matching request in its HTTP cache.
+	//
+	// 	If there is a match, fresh or stale, the browser will make a conditional
+	// 	request to the remote server. If the server indicates that the resource has
+	// 	not changed, it will be returned from the cache. Otherwise the resource will
+	// 	be downloaded from the server and the cache will be updated.
+	//
+	// 	If there is no match, the browser will make a normal request, and will
+	// 	update the cache with the downloaded resource.
 	NoCache
+
+	// ForceCache The browser looks for a matching request in its HTTP cache.
+	//
+	// 	If there is a match, fresh or stale, it will be returned from the cache.
+	//
+	// 	If there is no match, the browser will make a normal request, and will
+	// 	update the cache with the downloaded resource.
 	ForceCache
+
+	// OnlyIfCached The browser looks for a matching request in its HTTP cache.
+	//
+	// 	If there is a match, fresh or stale, if will be returned from the cache.
+	//
+	// 	If there is no match, the browser will respond with a 504 Gateway timeout
+	// 	status.
+	//
+	// The "only-if-cached" mode can only be used if the request's mode is
+	// "same-origin". Cached redirects will be followed if the request's redirect
+	// property is "follow" and the redirects do not violate the "same-origin"
+	// mode.
 	OnlyIfCached
 )
 
