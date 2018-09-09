4cc4b9c5fabb424258966bbeca8af23ff06bdbd8
gernest <geofreyernest@live.com>
2018-09-09 20:56:33 +0300

    add mode option if set

--- tafuta.go
+++ tafuta.go
@@ -418,6 +418,9 @@ func (c *Client) Do(req *Request) (res *Response, err error) {
 	if req.Header != nil {
 		opts["headers"] = req.Header.Value()
 	}
+	if mode := req.Mode.String(); mode != "" {
+		opts["mode"] = mode
+	}
 	if cache := req.Cache.String(); cache != "" {
 		opts["cache"] = cache
 	}
